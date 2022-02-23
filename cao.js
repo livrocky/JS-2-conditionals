cb8b7137df7e246653656702b1fdd181d1407551
rokas <rokasra@gmail.com>
2022-02-23 08:44:44 +0200

    cao masinu grupes veikia, login veikia

--- cao.js
+++ cao.js
@@ -1,42 +1,42 @@
 //1//
 
-const legalAge = 20;
-const clientAge = 19;
+// const legalAge = 20;
+// const clientAge = 19;
 
-if (clientAge >= legalAge) {
-  console.log("legalus amzius");
-} else if (clientAge < legalAge) {
-  console.log("nelegalus amzius");
-}
+// if (clientAge >= legalAge) {
+//   console.log("legalus amzius");
+// } else if (clientAge < legalAge) {
+//   console.log("nelegalus amzius");
+// }
 
 //2//
 
-const myName = "Rokassss";
+// const myName = "Rokassss";
 
-if (myName.length > 6) {
-  console.log("ilgas vardas");
-} else if (myName.length < 6) {
-  console.log();
-}
+// if (myName.length > 6) {
+//   console.log("ilgas vardas");
+// } else if (myName.length < 6) {
+//   console.log();
+// }
 
 //3//
 
-const myAge = 27;
+// const myAge = 27;
 
-if (myAge >= 100 || myAge <= 0) {
-  console.log("Invalid Age");
-} else if (myAge >= 1 && myAge <= 18) {
-  console.log("Child");
-} else if (myAge >= 19 && myAge <= 99) {
-  console.log("Adult");
-}
+// if (myAge >= 100 || myAge <= 0) {
+//   console.log("Invalid Age");
+// } else if (myAge >= 1 && myAge <= 18) {
+//   console.log("Child");
+// } else if (myAge >= 19 && myAge <= 99) {
+//   console.log("Adult");
+// }
 
 //4//
 
-const car = "BMW";
+const car = "Mini";
 
-if ((car === "VW", "Audi", "Bentley", "Bugatti", "Lamborghini", "Porsche")) {
+if (car === "VW" || car === "Audi" || car === "Bentley" || car === "Bugatti" || car === "Lamborghini" || car === "Porsche") {
   console.log("VW group");
-} else if ((car === "BMW", "Mini", "Rolls-Royce")) {
+} else if (car === "BMW" || car === "Mini" || car === "Rolls-Royce") {
   console.log("BMW group");
 }
--- login.js
+++ login.js
@@ -3,7 +3,8 @@ const pswd = prompt("Iveskite prisijungimo slaptazodi");
 
 if (username === "admin" && pswd === "master") {
   console.log("Sveiki, admin");
-} else if ((username !== "admin", pswd === username + 11)) {
-  console.log("Neteisingas slaptazodis!");
-} else username === null && pswd === null;
-console.log("Labankt!");
+} else if (pswd === username + 11) {
+  console.log("Teisingas slaptazodis!");
+} else if (!!username === false && !!pswd === false) {
+  console.log("Truksta duomenu");
+}
